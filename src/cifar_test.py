Fixed CIFAR test by normalizing pixel intensities

src/cifar_test.py
@@ -8,8 +8,7 @@ from mlp import MultilayerPerceptron
 if __name__ == '__main__':
     Xtrain, Ytrain, Xtest, Ytest = util.load_cifar('cifar-10-batches-py', reshape=True)
     Ytrain = util.one_hot(Ytrain, 10)
-    Xtest = Xtest[:1000]
-    Ytest = Ytest[:1000]
+
     net = ConvolutionalNetwork(
             input_shape=(3,32,32),
             num_out=10,
@@ -18,13 +17,8 @@ if __name__ == '__main__':
             hidden_sizes=[200,100],
             output_nl=lasagne.nonlinearities.softmax
     )
-    net.opt_setup(theano_stuff.mean_squared_error, lasagne.updates.adam)
-    # net = MultilayerPerceptron(
-    #         sizes=[3*32*32, 1000, 1000, 100, 10],
-    #         output_nl=lasagne.nonlinearities.softmax
-    # )
-    # net.opt_setup(theano_stuff.mean_squared_error, lasagne.updates.adam)
+    net.opt_setup(theano_stuff.cross_entropy_error, lasagne.updates.adam)
 
     while True:
-        net.train(Xtrain, Ytrain, itrs=100, batchsize=100)
+        net.train(Xtrain, Ytrain, itrs=50, batchsize=100)
         print('Accuracy:', net.evaluate(Xtest, Ytest, lambda y: np.argmax(y, axis=1)))

src/util.py
@@ -18,7 +18,7 @@ def load_cifar(dir, reshape=True):
         r = img[:1024].reshape(shape)
         g = img[1024:2048].reshape(shape)
         b = img[2048:].reshape(shape)
-        return np.stack([r,g,b])
+        return np.stack([r,g,b]) / 255.0
     if not reshape:
         reshape_image = lambda x: x
 