a78f2b2b2d7af7a9a1b326b14a034fdb8e2026e5
Garrett Thomas <gwthomas@berkeley.edu>
2016-12-08 03:59:10 +0100

    Whoops forgot to include main.py lol

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import lasagne
+import numpy as np
+
+import theano_stuff, util
+from visual_system import VisualSystem
+
+if __name__ == '__main__':
+    Xtrain, Ytrain, Xtest, Ytest = util.load_cifar('cifar-10-batches-py', reshape=False)#True)
+    Ytrain = util.one_hot(Ytrain, 10)
+
+    k = 3 # Number of recurrences
+    batchsize = 100
+    net = VisualSystem(k, batchsize)
+    net.compile()
+    net.opt_setup(theano_stuff.cross_entropy_error, lasagne.updates.adam)
+
+    while True:
+        net.train(Xtrain, Ytrain, itrs=50, batchsize=batchsize)
+        # For technical reasons we can only evaluate exactly batchsize samples at a time with this network
+        batchidx = np.random.permutation(len(Xtest))[:batchsize]
+        print('Accuracy:', net.evaluate(Xtest[batchidx], Ytest[batchidx], lambda y: np.argmax(y, axis=1)))
